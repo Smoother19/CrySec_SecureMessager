9ec345ff5de611ed66358d37753413268fa3cad8
Octalink <btfoctalink@gmail.com>
2026-02-24 13:51:56 +0100

    Mainwindow displays chat

--- Main.py
+++ Main.py
@@ -1,15 +1,25 @@
 from ConnectionHandler import *
 from MessageHandler import *
 import threading
+from PySide6.QtWidgets import QApplication
+from MainWindow import MainWindow
 
 try:
     connection = ConnectionHandler()
 except Exception as e:
     print(f"Error : {e}")
 
+app = QApplication()
+window = MainWindow().win
+window.show()
+
+app.exec()
+
+# Recieve messages from server on a separate thread
 receive_msg = threading.Thread(target=connection.receive_message)
 receive_msg.start()
 
 while True:
+    #Send message to server
     msg = input('Enter your message: ')
     connection.send_message(msg)
---
+++ MainWindow.py
@@ -0,0 +1,61 @@
+from PySide6.QtWidgets import *
+from PySide6.QtCore import Qt, QSize, QMargins
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.win = QWidget()
+
+        # window settings
+        self.win.setWindowTitle("CRYSEC - Secure Messager")
+        self.win.setFixedSize(QSize(700, 700))
+
+        mainBox = QHBoxLayout(self.win)
+        mainBox.setContentsMargins(QMargins(10, 10, 10, 10))
+
+        chatBox = QVBoxLayout() #Layout for chat
+        settingsBox = QVBoxLayout() # Layout for settings
+
+        # ScrollArea settings
+        scrollChat = QScrollArea(alignment=Qt.AlignmentFlag.AlignCenter)
+        scrollChat.setWidgetResizable(False)
+
+        # Set scroll to vertical not horizontal
+        scrollChat.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        scrollChat.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+
+        # ScrollArea Content
+        scrollContainer = QWidget()
+        scrollContainer.setFixedWidth(scrollChat.width())
+        self.scrollLayout = QVBoxLayout(scrollContainer)
+
+        #Example to add chat field to scrollArea
+        # for i in range(1, 200):
+        #     self.addChatField("wads")
+
+
+        scrollChat.setWidget(scrollContainer)
+        chatBox.addWidget(scrollChat)
+        
+        settingsBox.addWidget(scrollChat)
+
+        mainBox.addLayout(chatBox)
+        mainBox.addLayout(settingsBox)
+
+        self.setLayout(mainBox)
+    
+
+    def addChatField(self, chatText: str, isSend: bool=False):
+        label = QLabel(chatText)  
+        label.setFixedWidth(300)
+        label.setMargin(10)
+        label.setWordWrap(True)
+
+        if isSend:
+            label.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+            label.setStyleSheet("background-color: #CDFFC2; border-radius: 10px; color: black")  
+        else:
+            label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
+            label.setStyleSheet("background-color: #C9C9C9; border-radius: 10px; color: black")   
+        self.scrollLayout.addWidget(label) 
